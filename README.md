22d7905583b912903ec631d158b44437c03d3a24
dpodolskiy1990 <dpodolskiy1990@gmail.com>
2023-06-07 10:03:11 +0300

    file add

--- README.md
+++
@@ -1,5 +0,0 @@
-# delivery-app
-
-версія 0.0.1
-початок
-react, redux-toolkit, react-router-dom, scss
--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import mainSlice from "./Slice/mainSlice";
-const store = configureStore({
-  reducer: {
-    mainSlice,
-  },
-});
-
-export default store;
